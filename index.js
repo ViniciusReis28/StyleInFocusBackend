5fa3f1174df9b0725b361885b0336cecf0629f6a
Gabriel Pablo <pablobocudo.lima@gmail.com>
2024-11-12 22:54:22 -0300

    aaaaa

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ app.use('/camisas', camisasRoutes);  // Usando as rotas de camisas
 app.use('/users', usersRoutes);  // Usando as rotas de usuários
 app.use('/frete', freteRoutes);
 app.use('/api/roupas', comentarioRouter);
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/paginas/index/index.html'));
-  });
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
