6339cdc2c0c58218e693c514aed6aa6c0922d75f
Gabriel Pablo <pablobocudo.lima@gmail.com>
2024-11-12 22:50:50 -0300

    A

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const camisasRoutes = require('./routes/camisasRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 const freteRoutes = require('./routes/freteRoutes'); 
 const comentarioRouter = require('./routes/comentarioRouter');
+const path = require('path');
 require('dotenv').config();
 
 
@@ -17,7 +18,11 @@ app.use('/camisas', camisasRoutes);  // Usando as rotas de camisas
 app.use('/users', usersRoutes);  // Usando as rotas de usuários
 app.use('/frete', freteRoutes);
 app.use('/api/roupas', comentarioRouter);
+app.use(express.static(path.join(__dirname, '../frontend')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/paginas/index/index.html'));
+  });
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
